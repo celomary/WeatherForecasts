f71cba646e670d0cde699808631ad9ad801a15ba
Mohamed Elomary <elomarymohamed72@gmail.com>
2022-05-23 00:04:17 +0100

    remove heavy file

--- src/api/getWeatherByCityName.js
+++ src/api/getWeatherByCityName.js
@@ -1,9 +1,9 @@
 import { setWeather } from "../features/Weather";
-import getCityId from "../utils/getCityIdByName";
+/*import getCityId from "../utils/getCityIdByName"*/;
 
 
 const getWeatherByCity = async (dispatch, city)=>{
-    const data = await (await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city_id=${getCityId(city)}&key=${import.meta.env.VITE_API_KEY}`)).json();
+    const data = await (await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city_id=${445788/*getCityId(city)*/}&key=${import.meta.env.VITE_API_KEY}`)).json();
     dispatch(setWeather(data));
 }
 
